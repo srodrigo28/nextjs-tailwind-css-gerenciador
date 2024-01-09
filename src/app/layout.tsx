9c851899d28cdfda887dda86fe3252f664ea4060
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2024-01-09 09:05:22 -0300

    correcao-erro-toast-container-para-page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,7 +14,6 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="pt-BR">
-       <ToastContainer />
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Login from "./login/page";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
 
 export default function Home() {
   return (
     <main>
+        <ToastContainer />
         <Login />
     </main>
   )
